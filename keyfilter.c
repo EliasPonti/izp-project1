ad9a491139e8662a02b7a3b6e41edaf66f72588b
EliasPonti <lukus.el@gmail.com>
2023-10-15 17:23:20 +0200

    improvement #1

--- keyfilter.c
+++ keyfilter.c
@@ -6,24 +6,25 @@
 const int MAX_CHARS = 100;
 int validate_input(const char *input);
 void find_address(char *input);
-void input_upper(char *input);
+void str_to_uppercase(char *input);
 void remove_EOL(char *source);
 
 int main(int argc, char *argv[])
 {
-    char *input = argv[1];
-
     if (argc < 2)
     {
+        fprintf(stderr, "Error: input must be provided");
         return 1;
     }
+
+    char *input = argv[1];
 
     int errcode = validate_input(input);
     if (errcode != 0)
     {
         return errcode;
     }
-    input_upper(input);
+    str_to_uppercase(input);
     find_address(input);
 
     return 0;
@@ -31,7 +32,6 @@ int main(int argc, char *argv[])
 
 int validate_input(const char *input)
 {
-
     for (int i = 0; input[i] != '\0'; i++)
     {
         char ch = input[i];
@@ -44,9 +44,8 @@ int validate_input(const char *input)
     return 0;
 }
 
-void input_upper(char *input)
+void str_to_uppercase(char *input)
 {
-
     for (int i = 0; input[i]; i++)
     {
         input[i] = toupper(input[i]);
@@ -60,40 +59,35 @@ void find_address(char *input)
     char address_match[MAX_CHARS];
     next_characters[0] = '\0';
 
-    int input_len = strlen(input);
+    size_t input_len = strlen(input);
 
     while (fgets(address, sizeof(address), (stdin)) != NULL)
     {
-
-        for (int i = 0; address[i]; i++)
-        {
-            address[i] = toupper(address[i]);
-        }
+        str_to_uppercase(address);
 
         remove_EOL(address);
 
         char *result = strstr(address, input);
         if (result != NULL && *result == *address)
         {
-            if (strlen(input) == strlen(address))
+            strcpy(address_match, address);
+            char next_char[2] = "";
+            next_char[0] = address[input_len];
+            if (strstr(next_characters, next_char) == NULL)
             {
-                printf("Found: %s\n", address);
-                break;
+                strcat(next_characters, next_char);
             }
-            else
+
+            // exact match found
+            if (input_len > 0 && input_len == strlen(address))
             {
-                strcpy(address_match, address);
-                char next_char[2] = "";
-                next_char[0] = address[input_len];
-                if (strstr(next_characters, next_char) == NULL)
-                {
-                    strcat(next_characters, next_char);
-                }
+                strcat(next_characters, " ");
+                break;
             }
         }
     }
 
-    int next_characters_len = strlen(next_characters);
+    size_t next_characters_len = strlen(next_characters);
     if (next_characters_len == 0)
     {
         printf("Not found");
@@ -115,7 +109,6 @@ void find_address(char *input)
 void remove_EOL(char *source)
 {
     char *enter;
-
     if ((enter = strchr(source, '\n')) != NULL)
     {
         *enter = '\0';
